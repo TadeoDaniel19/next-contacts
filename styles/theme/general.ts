6b0b72f2577040a9025401b8333906862df5b8c0
Tadeo Daniel <tadeo@MacBook-Pro-de-Tadeo.local>
2022-08-04 20:15:56 -0500

    stls: add theme style for mui

---
+++ styles/theme/general.ts
@@ -0,0 +1,11 @@
+export const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '3em',
+  },
+  color: {
+    color: 'green',
+    backgroundColor: 'green',
+  }
+};
---
+++ styles/theme/theme.ts
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    background: {
+      default: "#ededed"
+    },
+    primary: {
+      main: '#E26376',
+    },
+    secondary: {
+      main: '#6F6E6B',
+    },
+  },
+});
